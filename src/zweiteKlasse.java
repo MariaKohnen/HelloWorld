7706ea9ad1a968a513b0f50c29a6ef41ec1c02d7
Maria Kohnen <maria.kohnen@gmail.com>
2022-03-22 14:15:36 +0100

    String_meinVorname

--- src/zweiteKlasse.java
+++ src/zweiteKlasse.java
@@ -1,6 +1,10 @@
-public class zweiteKlasse {
-
+public class helloWorld {
     public static void main(String[] args) {
-        System.out.println("Das ist eine zweite Klasse");
+        String myVor= "Maria";
+        String myNac= "Kohnen";
+        System.out.println("Hello Wolrd");
+        System.out.println(myVor + myNac);
+        System.out.println("Meine Vorname ist: " + myVor);
+
     }
 }
