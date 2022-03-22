caaab669f4a2c1059124b8f1ce33a80f2e40fb0a
Maria Kohnen <maria.kohnen@gmail.com>
2022-03-22 12:11:42 +0100

    Dritter Commit

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -1,5 +1,9 @@
 public class helloWorld {
     public static void main(String[] args) {
+        String myVor= "Maria";
+        String myNac= "Kohnen";
         System.out.println("Hello Wolrd");
+        System.out.println(myVor + myNac);
+
     }
 }
